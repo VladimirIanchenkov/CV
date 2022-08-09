1f27261cbab05e9a47b93503b53f9f04d67fd958
VladimirIanchenkov <ianchenkov.v@gmail.com>
2022-08-09 18:25:34 +0700

    добавляет главное фото

--- gulpfile.js
+++ gulpfile.js
@@ -96,7 +96,8 @@ const copy = (done) => {
   gulp.src([
   'source/fonts/*.{woff2,woff}',
   'source/*.ico',
-  'source/*.webmanifest'
+  'source/*.webmanifest',
+  'source/doc/*.pdf'
   ], {
   base: 'source'
   })
